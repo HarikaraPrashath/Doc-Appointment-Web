c1e3566df0f6321e248ce298bdaed47a97d8faa4
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-23 10:56:35 +0530

    ui setting and function are done

---
+++ app/doctors/doc-spl/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Doctor spl Page
+    </div>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // Use system preference for dark mode
+  darkMode: "media", // ← important
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
